8da50297f7bae11ee3bb6ea0289007cf4f304e75
Flug <flugv1@gmail.com>
2014-02-10 23:11:50 +0100

    development functions for message encryption

--- scripts/script.js
+++ scripts/script.js
@@ -8,18 +8,56 @@ $(document).ready(function() {
 	  //	$("#keySalt").append(new Option(item, i));
 	    
 	  
-	
-}
-  $("#storage").on("click", function(){
-  	console.log($("#salt").val().length);
-	var salts = JSON.parse(localStorage.salts || "null") || [];
-  	if($("#salt").val().length > 10)
-  	{
-  		salts.push($.trim(($("#salt").val())));	
-  	}
-  	localStorage.salts = JSON.stringify(salts);
-getSalts();
- });
+  }
+    $("#storage").on("click", function(){
+
+	  var salts = JSON.parse(localStorage.salts || "null") || [];
+	  if($("#salt").val().length > 10) {
+		  salts.push($.trim(($("#salt").val())));	
+	  }
+
+	  localStorage.salts = JSON.stringify(salts);
+
+      getSalts();
+   });
+
+  var Bits = 2048;
+  var PassPhrase = $('#keySalt option:selected').text();
+  var RSAkey  ;
+
+  $("#keySalt").change(function(){
+
+    PassPhrase = $('#keySalt option:selected').text();
+
+    RSAkey = cryptico.generateRSAKey(PassPhrase, Bits);
+
+  }).trigger("change");
+  console.log(PassPhrase);
+
+  var PublicKeyString = cryptico.publicKeyString(RSAkey);
+  var PlainText;
+  var EncryptionResult;
+  var DecryptionResult;
+  $('#message').bind("keypress keydown click keyup", function(e){
+    PlainText = this.value; 
+    EncryptionResult = cryptico.encrypt(PlainText, PublicKeyString);
+
+    DecryptionResult = cryptico.decrypt(EncryptionResult.cipher, RSAkey);
+
+
+    $("#encrypt").val(EncryptionResult.cipher);
+
+    $("#clair").val(DecryptionResult.plaintext);
+  });
+/*
+$("#decrypt").bind("keypress keydown click keyup", function(e){
+console.log(PassPhrase);
+
+  decrypt = cryptico.decrypt(this.value, RSAkey);
+  $("#done").val(decrypt.plaintext);
+});
+*/
+
 });
 
 
